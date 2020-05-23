c2dbb24c7e9238bbac054eff1a7a7de5917d1acf
acarcila <acarcila98@gmail.com>
2020-05-22 22:17:01 -0500

    Se realizó la interpretación y decodificación de JSON en el Host

---
+++ ProjectMultiplayerOnline/Assets/Model/Player.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Player
+{
+    public float positionX;
+    public float positionY;
+    public float positionZ;
+    public float rotationX;
+    public float rotationY;
+    public float rotationZ;
+    public int id;
+
+    public Player(float positionX, float positionY, float positionZ, float rotationX, float rotationY, float rotationZ, int id)
+    {
+        this.positionX = positionX;
+        this.positionY = positionY;
+        this.positionZ = positionZ;
+        this.rotationX = rotationX;
+        this.rotationY = rotationY;
+        this.rotationZ = rotationZ;
+        this.id = id;
+    }
+}
